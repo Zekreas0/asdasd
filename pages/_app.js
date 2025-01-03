e33730a56640714704b2909da655a92b310a1a95
Baran Sakçı <baransakci0@gmail.com>
2025-01-03 15:52:29 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,5 +3,17 @@ import '@styles/globals.css'
 function Application({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
-
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
 export default Application
